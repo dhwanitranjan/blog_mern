1c16de9254766e0348eebdc246d81b8ea9ce19fb
dhwanitrranjan <dhwanit.ranjan@techmojo.in>
2024-03-14 00:12:39 +0530

    navbar created with links to required pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+// import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
@@ -6,20 +6,23 @@ import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import Project from "./pages/Project";
 import About from "./pages/About";
+import Header from "./component/Header";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signip" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
-    // <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signip" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
